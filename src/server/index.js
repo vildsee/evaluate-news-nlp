0c95bd968ef7acae9b8f1a069b97a3aff161e925
vildsee <78222196+vildsee@users.noreply.github.com>
2021-10-15 15:20:46 +0200

    First install, server setup

--- src/server/index.js
+++ src/server/index.js
@@ -8,13 +8,21 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+const cors = require('cors')
+app.use(cors())
+
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
+const port = 8080;
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
+const server = app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
