f4e2317deee464ee7390cc1a74688757e9c44334
vildsee <78222196+vildsee@users.noreply.github.com>
2022-05-11 18:27:09 +0200

    npm i

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,14 +1,15 @@
 function checkForName(inputUrl) {
-    // console.log("::: Running checkForName :::", inputUrl);
-    var res = inputUrl.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    if(res == null){
-        console.log('false');
-        return false;
-    }
-        else {
-            console.log('true');
-            return true;
-    }
+  // console.log("::: Running checkForName :::", inputUrl);
+  let res = inputUrl.match(
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+  );
+  if (res == null) {
+    console.log("false");
+    return false;
+  } else {
+    console.log("true");
+    return true;
+  }
 }
 
-export { checkForName }
+export { checkForName };
